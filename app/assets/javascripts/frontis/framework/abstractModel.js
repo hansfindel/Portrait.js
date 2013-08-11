5405b12be801220859ce81146e5dee00c0116d55
Hans <hanstrax@gmail.com>
2013-08-11 17:28:51 -0400

    size validation on model

--- app/assets/javascripts/frontis/framework/abstractModel.js
+++ app/assets/javascripts/frontis/framework/abstractModel.js
@@ -44,5 +44,22 @@ AbstractModel = AbstractClass.extend({
 	requirePresenceOf: function(target, message){
 		this.validator.require_presence_of(target, message);
 	}, 
-	
+	requireEmailFormatOf: function(target, message){
+		this.validator.require_email_format_of(target, message);
+	}, 
+	requireUsernameFormatOf: function(target, message){
+		this.validator.require_username_format_of(target, message);
+	},
+	requireFormatOf: function(format, target, message){
+		this.validator.require_format_of(format, target, message);
+	}, 
+	requireLengthOf: function(min, max, target, message){
+		this.validator.require_length_validation_of(target, min, max, message)
+	}, 
+	requireMinLengthOf: function(min, target, message){
+		this.validator.require_length_validation_of(target, min, null, message)
+	}, 
+	requireMaxLengthOf: function(max, target, message){
+		this.validator.require_length_validation_of(target, null, max, message)
+	}
 })
